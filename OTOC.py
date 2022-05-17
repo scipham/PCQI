Small bug fixes to OTOC computation

OTOC.py
@@ -139,7 +139,8 @@ class OTOC:
             otoc_val = raw_otoc_val / (np.sum(temp_tot_global_phases_1) + np.sum(temp_tot_global_phases_2))
             
             from time import sleep
-            print("raw: ", raw_otoc_val, (np.prod(temp_tot_global_phases_1) + np.prod(temp_tot_global_phases_2)))
+            print("raw: ", raw_otoc_val, (np.prod(np.exp(1j*temp_tot_global_phases_1)) * np.prod(np.exp(1j*temp_tot_global_phases_2))))
+            print(temp_tot_global_phases_1, temp_tot_global_phases_2)
             sleep(1000)
             
             otoc_output.append(otoc_val)

TD_NQS_RBM.py
@@ -154,12 +154,11 @@ class TD_NQS_RBM(NQS_RBM):
                 old_ensemble_prob_amps = new_ensemble_prob_amps.copy()
             overlap_psi_t_psi_t_dt = self.RMB_inner_product(old_ensemble_prob_amps, new_ensemble_prob_amps)
             print("overlap t, dt:", overlap_psi_t_psi_t_dt)
-            evol_phase = np.angle(overlap_psi_t_psi_t_dt / ((1 - 1j*EExpVal)*delta_t )) / delta_t
+            evol_phase = np.angle(overlap_psi_t_psi_t_dt / ((1 - 1j*EExpVal*delta_t )))
             
             #Store the new weights and old probability amplitudes for next timestep
             self.weights = new_weights
             old_ensemble_prob_amps = new_ensemble_prob_amps.copy()
-            
             
             print(val_error, tdvp_error)
             

experiments/basic_otoc_exp.py
@@ -43,6 +43,8 @@ time_samples = np.array([0.002, 0.004, 0.006, 0.008]) #np.linspace(0.0, 1.0, 5)
 print(time_samples)
 
 otoc_output = otoc_calculator.compute_efficient(evol_params=evol_params,
-                                     time_samples=time_samples)
+\
+    
+    time_samples=time_samples)
 print(otoc_output[1])
 plt.plot(otoc_output[0], otoc_output[1])