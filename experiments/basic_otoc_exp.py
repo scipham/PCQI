2a57e757b9b526e9d4593c01dcd262638a3e83c1
Scipham <thomas.rothe@protonmail.com>
2022-05-22 13:10:24 +0000

    Update basic_otoc_exp.py

--- experiments/basic_otoc_exp.py
+++ experiments/basic_otoc_exp.py
@@ -25,12 +25,12 @@ init_state_params = {"kContrastDiv": 6000,
                      "lrate": 0.4, 
                      "epochs": 70}
 
-evol_params = {'delta_t': 0.001,
-            'end_of_time':0.005,
-            'kContrastDiv': 9000,
+evol_params = {'delta_t': 0.5e-3,
+            'end_of_time':0.4,
+            'kContrastDiv': 8000,
             'reg_mode':'diag_shift',
-            'reg_strength':0.0005,
-            'val_fraction':0.2}
+            'reg_strength':1e-3,
+            'val_fraction':0.1}
 
 otoc_calculator = OTOC(hamilt = hamilt,
             Nv = N_V,
@@ -39,12 +39,15 @@ otoc_calculator = OTOC(hamilt = hamilt,
             init_state_params=init_state_params)
 
 
-time_samples = np.array([0.002, 0.004, 0.006, 0.008]) #np.linspace(0.0, 1.0, 5)
+time_samples = np.arange(10*evol_params['delta_t'],evol_params['end_of_time'], 10*evol_params['delta_t'] ) #np.linspace(0.0, 1.0, 5)
 print(time_samples)
 
 otoc_output = otoc_calculator.compute_efficient(evol_params=evol_params,
-\
-    
-    time_samples=time_samples)
+                                                time_samples=time_samples)
 print(otoc_output[1])
+
+with open(RESULTS_PATH+"temp_otoc_result.pickle",'wb') as f:
+    pickle.dump(otoc_output,f)
+
+
 plt.plot(otoc_output[0], otoc_output[1])
